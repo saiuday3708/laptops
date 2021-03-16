7b1d8c04cf72cf8e32b653989e11051efb1e14c7
saiuday <saiuday3708@gmail.com>
2021-03-16 20:36:38 +0530

    lap commit

---
+++ src/main/java/lap/ApplicationUI.java
@@ -0,0 +1,30 @@
+package lap;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
+@SpringUI
+@Theme("applicationTheme")
+@Title("Vaading + Springboot")
+public class ApplicationUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest request) {
+
+        Label label = new Label("Welcome to Vaadin + Springboot");
+        label.setStyleName(ValoTheme.LABEL_H1);
+
+        VerticalLayout layout = new VerticalLayout();
+        layout.addComponent(label);
+        layout.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
+
+        setContent(layout);
+    }
+}
